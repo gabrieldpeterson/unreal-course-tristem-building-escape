b1193e2857bb42285c1deaa9ed7140b6e4b459d8
Gabriel Peterson <gabrieldpeterson@gmail.com>
2022-01-16 17:46:13 -0800

    Create a component that outputs the actor's location to the console

---
+++ Source/BuildingEscape/WorldPosition.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WorldPosition.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UWorldPosition::UWorldPosition()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UWorldPosition::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FString OwnerName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("Name: %s"), *OwnerName);
+
+	FString ObjectPosition = GetOwner()->GetActorLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("Location: %s"), *ObjectPosition);
+
+	FString ObjectPosition2 = GetOwner()->GetActorLocation().ToCompactString();
+	UE_LOG(LogTemp, Warning, TEXT("Location: %s"), *ObjectPosition2);
+}
+
+
+// Called every frame
+void UWorldPosition::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
